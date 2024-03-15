ebcf71c4d2e66b2f643dec61bbb76813ab5a3046
駒田隆人 <takato_2005@icloud.com>
2024-03-15 09:20:04 +0900

    2024/03/15 the secound commit

--- xlink/admin.py
+++ xlink/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Account, Group, Comment, Category,CommentCount, ReturnComment, Root, FollowCount, AccountRoot, FollowersCount
 # Register your models here.
+admin.site.register(Account) 
+admin.site.register(Comment) 
+admin.site.register(Category)
+admin.site.register(Group)
+admin.site.register(CommentCount)
+admin.site.register(ReturnComment)
+admin.site.register(Root)
+admin.site.register(FollowCount)
+admin.site.register(AccountRoot)
+admin.site.register(FollowersCount)
--- xlink/urls.py
+++ xlink/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # ... 他のURLパターン ...
     path('accounts/', views.index, name="index"),
     path('', views.home, name="home"),
+    path('profile_create/', views.form_create, name="profile_create"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
