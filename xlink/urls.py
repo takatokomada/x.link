1eebcc8c27343d868392ed8f23a0ec585aab320e
駒田隆人 <takato_2005@icloud.com>
2024-03-16 12:16:20 +0900

    2024/03/16 the third commit

--- xlink/urls.py
+++ xlink/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('profile_create/', views.form_create, name="profile_create"),
     path('class/create.html/', views.form_class, name="class_create"),
     path('community/<str:name>/', views.community, name="community"),
+    path('follow_count', views.follow_count, name="follow_count"),
+    path('root_selecter', views.root_selecter, name="root_selecter"),
+    path('follow_counts', views.follow_counts, name="follow_counts"),
+    path('root_count', views.root_count, name="root_count"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
